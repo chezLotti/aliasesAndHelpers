43a4af892a5c047ad78c7edf08cb10f363ede75c
Charlotte <139063874+chezLotti@users.noreply.github.com>
2025-05-15 13:46:24 +0200

    Update aliases.sh
    added "ip address" aliases
    added docker volume alias

--- aliases.sh
+++ aliases.sh
@@ -6,9 +6,16 @@ aliasFile=~/.bash_aliases;
 
 [ "$(grep -q 'alias la' $aliasFile; echo $?)" -eq 1 ] && echo "alias la='ls -la'" >> $aliasFile;
 
+[ "$(grep -q 'alias ips' $aliasFile; echo $?)" -eq 1 ] && echo "alias ips='ip address | grep \"inet\\s\"'" >> $aliasFile;
+
+[ "$(grep -q 'alias ipsv6' $aliasFile; echo $?)" -eq 1 ] && echo "alias ipsv6='ip address | grep inet6'" >> $aliasFile;
+
+### docker
 [ "$(grep -q 'alias dnls' $aliasFile; echo $?)" -eq 1 ] && echo "alias dnls='sudo docker network ls --format \"{{.ID}}\t{{.Driver}}\t{{.Scope}}\t{{.Name}}\"'" >> $aliasFile;
 
 [ "$(grep -q 'alias dils' $aliasFile; echo $?)" -eq 1 ] && echo "alias dils='sudo docker image ls'" >> $aliasFile;
+
+[ "$(grep -q 'alias dvls' $aliasFile; echo $?)" -eq 1 ] && echo "alias dvls='sudo docker volume ls'" >> $aliasFile;
 
 [ "$(grep -q 'function dcls' $aliasFile; echo $?)" -eq 1 ] &&
 cat <<EOF >> $aliasFile
